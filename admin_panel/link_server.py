69797fc357e5868552850c593a19fe71d55d13ee
alexe <aleksey.tolstukhin@school.msu.ru>
2021-10-15 17:48:48 +0300

    Проект от 15.10.2021 Реализация админ панели: создание и удаление корневых директорий. Задание: реализовать через командную строку движение в папку и обратно.

---
+++ admin_panel/link_server.py
@@ -0,0 +1,16 @@
+from ftplib import FTP
+
+ftp = FTP('server90.hosting.reg.ru', 'u1490660', passwd='Ds3Nb2d5wYj6UW28')
+ftp.login(user='u1490660', passwd='Ds3Nb2d5wYj6UW28')
+
+
+def add_folder(folder_name):
+    ftp.mkd(folder_name)
+
+
+def remove_folder(folder_name):
+    ftp.rmd(folder_name)
+
+
+def get_all_folders():
+    return list(filter(lambda x: not ('.' in x), ftp.nlst()))
---
+++ admin_panel/main.py
@@ -0,0 +1,76 @@
+from flask import Flask, render_template, request
+import link_server
+
+app = Flask(__name__)
+
+USER = 'admin'
+PSWRD = 'admin123'
+flag = False
+
+
+@app.route('/')
+def engine():
+    return render_template(
+        'admin.html',
+        flag=flag
+    )
+
+
+@app.route('/login/', methods=['POST'])
+def log_in_function():
+    user = request.form['user']
+    pswrd = request.form['pass']
+    if user == USER and pswrd == PSWRD:
+
+        folders = link_server.get_all_folders()
+
+        return render_template(
+            'admin_panel.html',
+            flag_create=False,
+            user=user,
+            dirs=folders
+        )
+    else:
+        flag = True
+        return render_template(
+            'admin.html',
+            flag=flag
+        )
+
+
+@app.route('/action_with_dir/', methods=['POST'])
+def action_with_dir():
+    print(request.form['action'])
+    folders = link_server.get_all_folders()
+
+    if request.form['action'] == 'create':
+        return render_template(
+            'admin_panel.html',
+            flag_create=True,
+            user=USER,
+            dirs=folders
+        )
+    elif request.form['action'] == 'ok':
+        folder_name = request.form['folder']
+        link_server.add_folder(folder_name)
+        folders = link_server.get_all_folders()
+        return render_template(
+            'admin_panel.html',
+            flag_create=False,
+            user=USER,
+            dirs=folders
+        )
+    elif request.form['action'] == 'delete':
+        folder_name = request.form['folder_names']
+        print(folder_name)
+        link_server.remove_folder(folder_name)
+        folders = link_server.get_all_folders()
+        return render_template(
+            'admin_panel.html',
+            flag_create=False,
+            user=USER,
+            dirs=folders
+        )
+
+
+app.run('127.0.0.1', 8080)
