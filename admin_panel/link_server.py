4b75d71da0d2a9799a9eb545a0687d1f1f17ac4d
xyliganchik2007 <yarikoval2512@mail.ru>
2021-11-03 15:16:39 +0300

    кнопка назад + список файлов

--- admin_panel/link_server.py
+++ admin_panel/link_server.py
@@ -16,9 +16,25 @@ def get_all_folders():
     return list(filter(lambda x: not ('.' in x), ftp.nlst()))
 
 
-def open_folder(folder):
-    ftp.cwd('./'+folder)
+def open_folder(folder_name):
+    ftp.cwd('./'+folder_name)
 
 
 def cur_folder():
     return ftp.pwd()
+
+
+def get_file_size(file_name):
+    return ftp.sendcmd(f'SIZE {file_name}')
+
+
+def get_all_files():
+    return list(filter(lambda x: '.' in x and not (x[0] == '.'), ftp.nlst()))
+
+
+def add_file(file):
+    ftp.sendcmd(f'STOR {file}')
+
+
+def download_file(file_name):
+    return ftp.sendcmd(f'RETR {file_name}')
