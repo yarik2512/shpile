ba7c8f9051999ba582986e5d6953f7f801120a00
KachkoBart <shv-04@mail.ru>
2021-11-03 15:22:47 +0300

    by Viktor 2

--- test_creator/create_test.py
+++ test_creator/create_test.py
@@ -39,95 +39,9 @@ def function():  # надо придумать адекватное назван
     # TODO реализовать нажатие кнопки закрыть, упаковать тест и загрузить в БД
 
 
-# Реализация Виктора Швец
 @app.route("/editor_one_chose/", methods=['POST'])
-def func_4_radio():
-    global TEST, LAST_ID
-    req = request.form['action']
-
-    if req == 'add_answer' and len(TEST[LAST_ID]['Q']) == 0:
-        question = request.form['question']
-        TEST[LAST_ID]['Q'] = question
-        return render_template(
-            'one_chose_editor.html',
-            question=TEST[LAST_ID]['Q'],
-            lenght=len(TEST[LAST_ID]['A']),
-            answers=TEST[LAST_ID]['A'],
-            point=0
-        )
-    elif req == 'add_answer':
-        temp = []
-        point = int(request.form['right-answer'])
-        for i in range(len(TEST[LAST_ID]['A'])):
-            answer = request.form[f"{i}"]
-            if point == i:
-                temp.append([answer, 100, True])
-            else:
-                temp.append([answer, 0, False])
-        TEST[LAST_ID]['A'] = temp
-        TEST[LAST_ID]['A'].append(['', 0])
-
-        return render_template(
-            'one_chose_editor.html',
-            question=TEST[LAST_ID]['Q'],
-            lenght=len(TEST[LAST_ID]['A']),
-            answers=TEST[LAST_ID]['A'],
-            point=int(point)
-        )
-    elif req == 'save':
-        temp = []
-        point = int(request.form['right-answer'])
-        for i in range(len(TEST[LAST_ID]['A'])):
-            answer = request.form[f"{i}"]
-            if point == i:
-                temp.append([answer, 100, True])
-            else:
-                temp.append([answer, 0, False])
-
-        TEST[LAST_ID]['A'] = temp
-        json_test = json.dumps(TEST[LAST_ID])
-        # update_tasks(USER, 'radio', json_test)
-        print(TEST)
-
-        # TODO №1 Исправить функцию сборки вопроса: собираем json по
-        #  вопросу, и отправляем его в БД. В словарь TEST теперь должен быть
-        #  таким: {id_теста: номер, test_title: название теста (у нас оно
-        #  нигде не вводится), tasks: список id-вопросов.
-
-        return render_template(
-            'one_chose_editor.html',
-            question=TEST[LAST_ID]['Q'],
-            lenght=len(TEST[LAST_ID]['A']),
-            answers=TEST[LAST_ID]['A'],
-            point=int(point)
-        )
-    elif 'close' in req:
-        temp = []
-        point = int(request.form['right-answer'])
-
-        for i in range(len(TEST[LAST_ID]['A'])):
-            answer = request.form[f"{i}"]
-            if point == i:
-                temp.append([answer, 100, True])
-            else:
-                temp.append([answer, 0, False])
-        TEST[LAST_ID]['A'] = temp
-        index = int(req.split('-')[1])
-        if index == int(point):
-            point = 0
-        TEST[LAST_ID]['A'].pop(index)
-        return render_template(
-            'one_chose_editor.html',
-            question=TEST[LAST_ID]['Q'],
-            lenght=len(TEST[LAST_ID]['A']),
-            answers=TEST[LAST_ID]['A'],
-            point=int(point)
-        )
-    elif req == 'cls-editor':
-        return render_template(
-            'test_editor.html',
-            obj=TEST
-        )
+def edit_multiple_choice():
+    return temp_file.func_4_radio(TEST, TASK, request)
 
 
 #
@@ -148,4 +62,4 @@ def edit_multiple_choice():
     return temp_file.edit_multiple_choice(TEST, TASK, request)
 
 
-app.run('127.0.0.1', 8090, debug=True)
+app.run('127.0.0.1', 8088, debug=True)
--- test_creator/temp_file.py
+++ test_creator/temp_file.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 import json
+import link
 
 
 def engine(test):
@@ -66,11 +67,11 @@ def edit_multiple_choice(test, task, req):
             return rendering_multiple_choice(task)
         else:
             test['tasks'].append(task)
+            #link.update_tasks('User', 'multi', json_test)
             # TODO загрузить task в БД
             return engine(test)
     elif r == 'close-without-save':
         return engine(test)
-
     elif r == 'cls-editor':
         if task['flag'] != 3:
             task['Q'] = req.form['question']
@@ -81,6 +82,7 @@ def edit_multiple_choice(test, task, req):
                 task['A'][i][1] = weight
             task['flag'] = -1
         test['tasks'].append(task)
+        #link.update_tasks('User', 'multi', json_test)
         # TODO загрузить task в БД
         return engine(test)
     else:
@@ -96,3 +98,86 @@ def edit_multiple_choice(test, task, req):
         task['A'].pop(index)
 
         return rendering_multiple_choice(task)
+
+
+def func_4_radio(test, task, req):
+    r = req.form['action']
+    if r == 'add_answer' and not task['Q']:
+        if not req.form['question']:
+            task['flag'] = 1
+            return rendering_one_choice(task)
+        else:
+            task['Q'] = req.form['question']
+            task['A'].append(['', 0])
+            return rendering_one_choice(task)
+    elif r == 'add_answer':
+        temp = []
+        point = int(req.form['right-answer'])
+        for i in range(len(task['A']) - 1):
+            answer = req.form[f"{i}"]
+            if point == i:
+                temp.append([answer, 100, True])
+            else:
+                temp.append([answer, 0, False])
+        task['A'] = temp
+        task['A'].append(['', 0])
+        return rendering_one_choice(task)
+    elif r == 'save' or r == 'close-with-save':
+        temp = []
+        point = int(req.form['right-answer'])
+        for i in range(len(task['A']) - 1):
+            answer = request.form[f"{i}"]
+            if point == i:
+                temp.append([answer, 100, True])
+            else:
+                temp.append([answer, 0, False])
+
+        task['A'] = temp
+        json_test = json.dumps(task)
+        task['flag'] = 3
+        print(task)
+        if r == 'save':
+            return rendering_one_choice(task)
+        else:
+            test['tasks'].append(task)
+            # link.update_tasks('User', 'multi', json_test)
+            # TODO загрузить task в БД
+            return engine(test)
+        # TODO №1 Исправить функцию сборки вопроса: собираем json по
+        #  вопросу, и отправляем его в БД. В словарь TEST теперь должен быть
+        #  таким: {id_теста: номер, test_title: название теста (у нас оно
+        #  нигде не вводится), tasks: список id-вопросов.
+    elif r == 'close-without-save':
+        return engine(test)
+    elif r == 'cls-editor':
+        if task['flag'] != 3:
+            temp = []
+            point = int(req.form['right-answer'])
+            task['Q'] = req.form['question']
+            for i in range(len(task['A']) - 1):
+                answer = req.form[f"{i}"]
+                if point == i:
+                    temp.append([answer, 100, True])
+                else:
+                    temp.append([answer, 0, False])
+            task['flag'] = -1
+        test['tasks'].append(task)
+        #link.update_tasks('User', 'multi', json_test)
+        # TODO загрузить task в БД
+        return engine(test)
+    elif 'close' in r:
+        temp = []
+        point = int(request.form['right-answer'])
+
+        for i in range(len(task['A']) - 1):
+            answer = request.form[f"{i}"]
+            if point == i:
+                temp.append([answer, 100, True])
+            else:
+                temp.append([answer, 0, False])
+        task['A'] = temp
+        index = int(req.split('-')[1])
+        if index == int(point):
+            point = 0
+        task['A'].pop(index)
+        return rendering_one_choice(task)
