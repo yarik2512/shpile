2435d89eff9b5cdec9cf036952dce82541121ee9
alexei_v_tolstukhin <tolstukhin.av@sch2.ru>
2022-02-18 18:15:12 +0300

    Начало сборки полного проекта

--- welldone/Connections.py
+++ welldone/Connections.py
@@ -2,7 +2,7 @@ from mysql.connector import connect
 from ftplib import FTP
 
 
-def getConnectionDB():
+def get_connection_db():
     con = connect(
         host='37.140.192.174',
         database='u1490660_default',
@@ -12,8 +12,7 @@ def getConnectionDB():
     return con
 
 
-def getFTP():
+def get_ftp():
     ftp = FTP('server90.hosting.reg.ru', 'u1490660', passwd='Ds3Nb2d5wYj6UW28')
     ftp.login(user='u1490660', passwd='Ds3Nb2d5wYj6UW28')
     return ftp
-
