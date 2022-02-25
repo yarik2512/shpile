b938ed533ec06eb94be8b1571c85d51a0df2869d
alexei_v_tolstukhin <tolstukhin.av@sch2.ru>
2022-02-25 16:53:59 +0300

    Сборка проекта: 25.02.22

---
+++ welldone/load_account.py
@@ -0,0 +1,25 @@
+from flask import Flask, request, render_template
+
+ID = 1
+
+app = Flask(__name__)
+
+
+@app.route('/actions', methods=['POST'])
+def make_some_action():
+    act = request.form['action']
+    print(act)
+    return render_template(
+        'account.html'
+    )
+
+
+@app.route('/')
+def load_page_account():
+    return render_template(
+        'account.html'
+    )
+
+
+if __name__ == '__main__':
+    app.run('127.0.0.1', 8080, debug=True)
