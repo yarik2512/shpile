951defffc76fb45b03d34d1f676ada6abe5031bd
xyliganchik2007 <yarikoval2512@mail.ru>
2022-03-16 19:16:24 +0300

    отображение данных пользователя в лк

--- welldone/account/load_account.py
+++ welldone/account/load_account.py
@@ -19,23 +19,77 @@ def get_info():
             f"SELECT id_groups FROM `students` WHERE id='{ID}'"
         )
         res = cur.fetchall()
-        groups_id = res[0][0].split(';')
+        if len(res) == 0:
+            res = ''
+        else:
+            res = res[0][0]
+        groups_id = res.split(';')
         groups = list()
         for group in groups_id:
             cur.execute(
                 f"SELECT name FROM `groups` WHERE id='{group}'"
             )
-            groups.append(cur.fetchall()[0][0])
+            res = cur.fetchall()
+            if len(res) == 0:
+                continue
+            groups.append(res[0][0])
         for group in groups:
             if '0' <= group[0] <= '9':
                 level = group
                 break
-    return ID, name, role, surname, name, secondname, level
+    courses = list()
+    if role == 'student':
+        cur.execute(
+            f"SELECT id_courses FROM `students` WHERE id='{ID}'"
+        )
+        res = cur.fetchall()
+        if len(res) == 0:
+            res = []
+        else:
+            res = res[0][0].split(';')
+        id_courses = list()
+        for r in res:
+            id_courses.append(r)
+        for id in id_courses:
+            tmp = dict()
+            tmp['id'] = id
+            cur.execute(
+                f"SELECT name FROM `courses` WHERE id='{id}'"
+            )
+            res = cur.fetchall()
+            if len(res) == 0:
+                continue
+            tmp['name'] = res[0][0]
+            courses.append(tmp)
+    elif role == 'teacher':
+        cur.execute(
+            f"SELECT id_courses FROM `teachers` WHERE id='{ID}'"
+        )
+        res = cur.fetchall()
+        if len(res) == 0:
+            res = []
+        else:
+            res = res[0][0].split(';')
+        id_courses = list()
+        for r in res:
+            id_courses.append(r)
+        for id in id_courses:
+            tmp = dict()
+            tmp['id'] = id
+            cur.execute(
+                f"SELECT name FROM `courses` WHERE id='{id}'"
+            )
+            res = cur.fetchall()
+            if len(res) == 0:
+                continue
+            tmp['name'] = res[0][0]
+            courses.append(tmp)
+    return ID, name, role, surname, name, secondname, level, courses
 
 
 def make_some_action():
     global con, cur
-    ID, name, role, surname, name, secondname, level = get_info()
+    ID, name, role, surname, name, secondname, level, courses = get_info()
     act = request.form['action']
     if act == 'add-material':
         return materials_add.engine()
@@ -52,13 +106,14 @@ def make_some_action():
         surname=surname,
         secondname=secondname,
         role=role,
-        level=level
+        level=level,
+        courses=courses
     )
 
 
 def load_page_account():
     global con, cur
-    ID, name, role, surname, name, secondname, level = get_info()
+    ID, name, role, surname, name, secondname, level, courses = get_info()
     return render_template(
         'account.html',
         id=ID,
@@ -66,5 +121,6 @@ def load_page_account():
         surname=surname,
         secondname=secondname,
         role=role,
-        level=level
+        level=level,
+        courses=courses
     )
