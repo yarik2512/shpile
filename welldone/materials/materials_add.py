ad18f81077e1ee97e01e3e7cc9214387927412c6
xyliganchik2007 <yarikoval2512@mail.ru>
2022-03-23 17:50:08 +0300

    добавление материала

--- welldone/materials/materials_add.py
+++ welldone/materials/materials_add.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, session
 from werkzeug.utils import secure_filename
+import os
 
 from welldone import link_server
 from welldone import db_functions
@@ -26,7 +27,8 @@ def add_material():
     data['subject'] = request.form['subject']
     data['file'] = request.files['file']
     data['link'] = 'materials/' + data['level'] + '/' + data['subject'] + '/' + secure_filename(data['file'].filename)
-    link_server.add_file(data['file'], '/www/shpile.space/' + data['link'])
+    # link_server.add_file(data['file'], '/welldone/' + data['link'])
+    data['file'].save(os.path.join(data['link']))
     db_functions.materials_add(data)
     materials = db_functions.materials_get_by_author(ID)
     return render_template(
